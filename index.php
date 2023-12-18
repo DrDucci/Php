219235f01e2fa98fdbac663424b210e62505315b
DrDucci <73859589+DrDucci@users.noreply.github.com>
2023-12-18 12:12:48 +0100

    La till css och fixade

--- index.php
+++ index.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="Rickpat.css">
 </head>
 <body>
-    Nisse!
+<form action=”index.php” method=”post”>
+
+    <input type=”text” name=”name” id=”name” required placeholder=”Skriv ditt namn”></input>
+    <input type=”number” name=”num” id=”num”></input>
+    <input type=”checkbox” name=”check” id=”check”></input>
+
+    <textarea name=”name” id=”text” cols=”30” rows=”10”></textarea>
+    <button type=”submit” value=”skicka” name=”btn”>tryck här!!</button>
+</form>
+
 </body>
 </html>
