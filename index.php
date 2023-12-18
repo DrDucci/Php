be4b25595279c0289dce160c1b8809e9c573d499
DrDucci <73859589+DrDucci@users.noreply.github.com>
2023-12-18 12:53:59 +0100

    Fixar lite mer css

--- index.php
+++ index.php
@@ -4,17 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="Rickpat.css">
+    <link rel="stylesheet" href="rickpat.css">
 </head>
 <body>
 
 <form action=”index.php” method=”post”>
-    <input type=”text” name=”name” id=”name” required placeholder=”Skriv ditt namn”></input>
+    <label for="name">Hej</label>
+    <input type=”text” name=”name” id=”name” required placeholder=Skriv ditt namn”></input>
+
+    <label for="num">Nummer</label>
     <input type=”number” name=”num” id=”num”></input>
+
+    <label for="check">Ja eller nej?</label>
     <input type=”checkbox” name=”check” id=”check”></input>
 
-    <textarea name=”name” id=”text” cols=”30” rows=”10”>Skriv här om du kan</textarea>
-    <button type=”submit” value=”skicka” name=”btn”>tryck här!!</button>
+    <textarea name=”name” id=”text” cols=”30” rows=”20”>Skriv här om du kan</textarea>
+    <button type=”submit” value=”skicka” name=”btn”>Skicka?</button>
 </form>
 
 </body>
